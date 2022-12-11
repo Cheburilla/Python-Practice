3e4651e6e9033ebc0602f90b0ad25748819e251f
Alexander <sanya.safron2003@gmail.com>
2022-12-11 12:44:10 +0600

    fixed mistakes

--- task1/sleight_of_hand.py
+++ task1/sleight_of_hand.py
@@ -1,7 +1,7 @@
-def sleight_of_hand(k: int, field: str) -> str:
+def sleight_of_hand(k: int, field: str) -> int:
     able = k*2
     counter = 0
     for k in range(1, 10):
         if field.count(str(k)) <= able and field.count(str(k)) != 0:
             counter += 1
-    return str(counter)
+    return counter
